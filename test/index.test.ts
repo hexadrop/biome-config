3e714f0f20e7ae24703220f1d4599a6fed6d4e36
MarioMH8 <mhidalgomario@gmail.com>
2025-11-13 23:21:58 +0100

    test: add snapshot tests for biome check functionality

---
+++ test/index.test.ts
@@ -0,0 +1,51 @@
+import { afterAll, beforeAll, describe, expect, test } from "bun:test";
+import { cp, mkdtemp, readdir, rm } from "node:fs/promises";
+import path from "node:path";
+import temporaryDirectory from "temp-dir";
+
+const fixtures = path.join(process.cwd(), "fixtures");
+const fixturesFiles = await readdir(fixtures);
+
+describe("@hexadrop/biome-config", () => {
+	let temporary: string;
+
+	beforeAll(async () => {
+		temporary = await mkdtemp(temporaryDirectory);
+		await cp(fixtures, temporary, {
+			recursive: true,
+		});
+	});
+
+	test.each(fixturesFiles)("biome check %s", async (file) => {
+		const temporaryFile = path.join(temporary, file);
+		const subprocess = Bun.spawn(
+			[
+				"bun",
+				"biome",
+				"check",
+				"--write",
+				temporaryFile,
+			],
+			{
+				cwd: process.cwd(),
+			},
+		);
+
+		const result = await subprocess.exited;
+
+		expect(result).toBe(0);
+
+		const temporaryFileContent = await Bun.file(temporaryFile).text();
+
+		expect(temporaryFileContent).toMatchSnapshot();
+	});
+
+	afterAll(async () => {
+		if (temporary) {
+			await rm(temporary, {
+				force: true,
+				recursive: true,
+			});
+		}
+	});
+});
